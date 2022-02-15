Fixed computation of PCA extreme points

masks_manipulation/relevant_points.py
@@ -11,8 +11,9 @@ def extract_centers(matrices):
     centers = np.zeros((n_masks, matrices.shape[0], 2)).astype(int)
     for value in range(1, n_masks+1):
         blob_coords = np.argwhere(matrices == value)
-        for n_step in range(matrices.shape[0]):
-            centers[value - 1, n_step] = blob_coords[blob_coords[:, 0] == n_step, 1:].mean(axis=0)
+        for n_frame in range(matrices.shape[0]):
+            centers[value - 1, n_frame] = blob_coords[
+                blob_coords[:, 0] == n_frame, 1:].mean(axis=0)
     return centers
 
 
@@ -25,26 +26,47 @@ def extract_extreme_points(matrices):
     center.
     '''
 
-    # TODO: Adapt this code to work in general (for now it is cut from the
-    # trials script, so the variables don't have a proper name and some of them
-    # have to be computed). Also, this is done only for one mask, it should be
-    # generalized for the number of masks contained in the matrices (usually 2))
-
-    # PCA for finger
-    first_finger = np.argwhere(matrices == 1)
-    first_frame = first_finger[first_finger[:,0] == 0][:, 1:]
-    center = centers[0,0]
+    n_masks = matrices.max().astype(int)
+    centers = extract_centers(matrices)
+    extreme_points = np.zeros((n_masks, matrices.shape[0], 4, 2)).astype(int)
     pca = sklearn.decomposition.PCA(2)
-    pca.fit(first_frame)
-    plt.imshow(masks[0])
-    for i, (comp, var) in enumerate(zip(pca.components_, pca.explained_variance_)):
-        min_val = first_frame[:, 1-i].min()
-        max_val = first_frame[:, 1-i].max()
-        plt.plot(
-            [center[1] + (min_val - center[1-i])*comp[1], center[1] + (max_val - center[1-i])*comp[1]],
-            [center[0] + (min_val - center[1-i])*comp[0], center[0] + (max_val - center[1-i])*comp[0]],
-            label=f"Component {i}",
-            linewidth=1,
-            color=f"C{i + 3}",
-        )
-    plt.show()
+
+    for idx in range(n_masks):
+        finger = idx+1
+        finger_centers = centers[idx]
+        finger_indices = np.argwhere(matrices == finger)
+        for n_frame in range(matrices.shape[0]):
+            frame_indices = finger_indices[finger_indices[:, 0] == n_frame, 1:]
+            finger_center = finger_centers[n_frame]
+
+            # Calculate direction of maximum variance (X-axis or Y-axis)
+            min_x = frame_indices[:, 0].min()
+            max_x = frame_indices[:, 0].max()
+            min_y = frame_indices[:, 1].min()
+            max_y = frame_indices[:, 1].max()
+            if max_x - min_x > max_y - min_y:
+                max_gt, min_gt = max_x, min_x
+                max_lt, min_lt = max_y, min_y
+                center_gt, center_lt = finger_center[0], finger_center[1]
+            else:
+                max_gt, min_gt = max_y, min_y
+                max_lt, min_lt = max_x, min_x
+                center_gt, center_lt = finger_center[1], finger_center[0]
+
+            # Fit PCA and get max and min variance directions
+            pca.fit(frame_indices)
+            var_gt = pca.components_[0]
+            var_lt = pca.components_[1]
+
+            extreme_points[idx, n_frame] = [
+                [finger_center[0] + (min_gt - center_gt)*var_gt[0],
+                 finger_center[1] + (min_gt - center_gt)*var_gt[1]],
+                [finger_center[0] + (max_gt - center_gt)*var_gt[0],
+                 finger_center[1] + (max_gt - center_gt)*var_gt[1]],
+                [finger_center[0] + (min_lt - center_lt)*var_lt[0],
+                 finger_center[1] + (min_lt - center_lt)*var_lt[1]],
+                [finger_center[0] + (max_lt - center_lt)*var_lt[0],
+                 finger_center[1] + (max_lt - center_lt)*var_lt[1]],
+            ]
+
+    return extreme_points.astype(int)
7e91872529a5b8c1c39783ea120d470697707f87
Francisco Luque Sánchez <fluque1995@gmail.com>
2022-03-01 13:34:14 +0100

    Changed temporal_window default from -1 to None

--- masks_manipulation/movement_index.py
+++ masks_manipulation/movement_index.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def movement_index(masks, temporal_window=-1):
+def movement_index(masks, temporal_window=None):
     """Calculate the amount of movement in the fingers, calculated as the ratio of
     the total space visited by each finger during the whole video and the mean
     space occupied by that finger in each frame. This method also allows to perform
@@ -10,15 +10,15 @@ def movement_index(masks, temporal_window=-1):
 
     Keyword arguments:
     masks           -- np.array with the result of segmentation
-    temporal_window -- Number of frames to consider in each temporal window. Use
-                       -1 to calculate the result for the whole video (default)
+    temporal_window -- Number of frames to consider in each temporal window. If
+                       None, calculate the result for the whole video (default None)
 
     """
     different_values = np.unique(masks)
     different_values = different_values[different_values != 0]
     results = {}
     for value in different_values:
-        if temporal_window != -1:
+        if temporal_window is None:
             curr_finger_movements = []
             for i in range(0, masks.shape[0], temporal_window):
                 curr_masks = masks[i: i+temporal_window]

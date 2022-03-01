ec2a126d64f0e08bbbdbfdb29b58b5db0e055297
Francisco Luque Sánchez <fluque1995@gmail.com>
2022-03-01 13:36:21 +0100

    Trials on fourier transform for frequency calculation

---
+++ calculate_fourier_transforms.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pickle as pkl
+
+from masks_manipulation import extract_centers, frequency_and_magnitude
+from visualization.plotting import plot_movements
+
+
+patient = "results/P1"
+experiment = 'Dedos_enfrentados'
+
+results = []
+
+centers = []
+
+for visit in ['Visita_1_OFF', 'Visita_2_ON', 'Visita_3_ON']:
+    with open(os.path.join(patient, visit, experiment, "masks.pkl"), "rb") as f:
+        masks = pkl.load(f)
+
+    curr_centers = extract_centers(masks, normalize=True, move_to_origin=True)
+    centers.append(curr_centers)
+    results.append(frequency_and_magnitude(curr_centers, fps=30))
+
+for center in centers:
+    plot_movements(center)

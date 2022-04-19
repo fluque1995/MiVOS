ca2cfc8ee97c5fc94bdfc252e56105a309de2f21
Francisco Luque Sánchez <fluque1995@gmail.com>
2022-04-19 09:17:54 +0200

    Fixed import

--- model/s2m/s2m_mobilenet.py
+++ model/s2m/s2m_mobilenet.py
@@ -1,5 +1,5 @@
 from torch import nn
-from torchvision.models.utils import load_state_dict_from_url
+from torch.utils.model_zoo import load_url as load_state_dict_from_url
 import torch.nn.functional as F
 
 __all__ = ['MobileNetV2', 'mobilenet_v2']

9dd1ffc6b98fd429238321c474dd12474fba0eff
Francisco Luque Sánchez <fluque1995@gmail.com>
2022-04-22 12:40:52 +0200

    Modified script to select if generated video shows center or extreme points of fingers

--- generate_masks_video.py
+++ generate_masks_video.py
@@ -2,22 +2,41 @@ import os
 import io_utils
 
 from visualization import matrices_to_video
-from masks_manipulation import extract_extreme_points
+from masks_manipulation import extract_extreme_points, extract_centers
 
-path = '../Mascaras/P2/Visita_1_OFF/Suero_izq'
-output_vid = 'output_der.mp4'
+centers_video = True
 
-masks = io_utils.load_masks(os.path.join(path, 'masks.pkl'))
-extreme_points = extract_extreme_points(masks)
+path = '../Mascaras/P2/Visita_2_ON/Dedos_enfrentados'
+output_vid = 'output_P2.mp4'
 
-for i, mask in enumerate(masks):
-    curr_frame_points = extreme_points[:, i, ...]
+if centers_video:
 
-    for k, pt in enumerate(curr_frame_points.reshape(8, 2)):
-        if pt[0]+1 < mask.shape[0] and pt[1]+1 < mask.shape[1]:
-            for i in range(-1, 2):
-                for j in range(-1, 2):
-                    mask[pt[0].astype(int)+i, pt[1].astype(int)+j] = 3+(k % 4)
+    masks = io_utils.load_masks(os.path.join(path, 'masks.pkl'))
+    centers = extract_centers(masks)
 
+    for i, mask in enumerate(masks):
+        curr_frame_points = centers[:, i, ...]
 
-matrices_to_video(masks, output_vid)
+        for k, pt in enumerate(curr_frame_points.reshape(2, 2)):
+            if pt[0]+1 < mask.shape[0] and pt[1]+1 < mask.shape[1]:
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        mask[pt[0].astype(int)+i, pt[1].astype(int)+j] = 3+k
+
+    matrices_to_video(masks, output_vid)
+
+else:
+
+    masks = io_utils.load_masks(os.path.join(path, 'masks.pkl'))
+    extreme_points = extract_extreme_points(masks)
+
+    for i, mask in enumerate(masks):
+        curr_frame_points = extreme_points[:, i, ...]
+
+        for k, pt in enumerate(curr_frame_points.reshape(8, 2)):
+            if pt[0]+1 < mask.shape[0] and pt[1]+1 < mask.shape[1]:
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        mask[pt[0].astype(int)+i, pt[1].astype(int)+j] = 3+(k % 4)
+
+    matrices_to_video(masks, output_vid)
